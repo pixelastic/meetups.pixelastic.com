580337fb57f6a2b8ec1f0130a453e3ee4399054e
Tim Carry <tim@pixelastic.com>
2021-12-22 16:07:46 +0100

    add youtube tag

---
+++ app/_plugins/youtube.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  class YoutubeTag < Liquid::Tag
+    def initialize(tag_name, value, tokens)
+      super
+      video_id = CGI.parse(URI.parse(value).query)['v'][0]
+      @embed_url = "http://www.youtube.com/embed/#{video_id}"
+    end
+
+    def render(_context)
+      <<~MARKUP.strip
+        <iframe class="youtube-player" type="text/html" width="640" height="390" src="#{@embed_url}" frameborder="0" allowfullscreen></iframe>
+      MARKUP
+    end
+  end
+end
+
+Liquid::Template.register_tag('youtube', Jekyll::YoutubeTag)
